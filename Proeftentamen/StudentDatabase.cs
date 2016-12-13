f6d780f8d88866bb643b2cfa67cf25f794538f1e
Sven Mark Hofstra <hofs1600@student.nhl.nl>
2016-12-13 18:09:08 +0100

    First commit

---
+++ Proeftentamen/StudentDatabase.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//Dit bestand mag niet aangepast worden! 
+namespace Proeftentamen
+{
+    public class Student
+    {
+        public int StudentNr { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Course
+    {
+        public int VakNr { get; set; }
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+    }
+
+    public class Exam
+    {
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+        public decimal Score { get; set; }
+    }
+
+    public class StudentDatabase
+    {
+        private static Student jan = new Student() { StudentNr = 1, Name = "Jan" };
+        private static Student piet = new Student() { StudentNr = 2, Name = "Piet" };
+        private static Student klaas = new Student() { StudentNr = 3, Name = "Klaas" };
+        private static Student katrijn = new Student() { StudentNr = 4, Name = "Katrijn" };
+
+        public static List<Student> Students = new List<Student>() { jan, piet, klaas, katrijn };
+
+        private static Course cSharp = new Course() { VakNr = 1, Name = "C#", Teacher = "Joris" };
+        private static Course math = new Course() { VakNr = 2, Name = "Wiskunde", Teacher = "Jos" };
+        private static Course coo = new Course() { VakNr = 3, Name = "Computer Organisation", Teacher = "Sibbele" };
+        private static Course se = new Course() { VakNr = 4, Name = "Software Engineering", Teacher = "David" };
+        private static Course python = new Course() { VakNr = 5, Name = "Python", Teacher = "Wouter" };
+
+        public static List<Exam> Exams = new List<Exam>() {
+            new Exam() { Student = jan,       Course = math,      Score = 3 },
+            new Exam() { Student = piet,      Course = math,      Score = 5 },
+            new Exam() { Student = jan,       Course = coo,       Score = 7 },
+            new Exam() { Student = klaas,     Course = cSharp,    Score = 9 },
+            new Exam() { Student = jan,       Course = cSharp,    Score = 5 },
+            new Exam() { Student = jan,       Course = math,      Score = 6 },
+            new Exam() { Student = katrijn,   Course = cSharp,    Score = 6 },
+            new Exam() { Student = piet,      Course = math,      Score = 8 },
+            new Exam() { Student = piet,      Course = coo,       Score = 5 },
+            new Exam() { Student = katrijn,   Course = se,        Score = 8 },
+            new Exam() { Student = katrijn,   Course = se,        Score = 9 }
+        };
+
+        public static List<Course> Courses = new List<Course>() {
+            cSharp, math, coo, se, python
+        };
+
+
+        //Opmerking: dit gaat goed, maar dan ook alleen maar omdat geen enkele sleutel duplicaten heeft!
+        private static Dictionary<int, Course> courseByVakNr = Courses.ToDictionary(crs => crs.VakNr);
+        private static Dictionary<string, Course> courseByName = Courses.ToDictionary(crs => crs.Name);
+        private static Dictionary<string, Course> courseByTeacher = Courses.ToDictionary(crs => crs.Teacher);
+
+        private static Dictionary<string, Student> studentByName = new Dictionary<string, Student>()
+        {
+            [jan.Name] = jan,
+            [piet.Name] = piet,
+            [klaas.Name] = klaas,
+            [katrijn.Name] = katrijn
+        };
+
+        private static Dictionary<int, Student> studentByStudentNr = new Dictionary<int, Student>()
+        {
+            [jan.StudentNr] = jan,
+            [piet.StudentNr] = piet,
+            [klaas.StudentNr] = klaas,
+            [katrijn.StudentNr] = katrijn
+        };
+
+        private static Dictionary<Student, List<Exam>> examsByStudent = CreateExamsByStudent();
+        private static Dictionary<Course, List<Exam>> examsByCourse = CreateExamsByCourse();
+        private static Dictionary<decimal, List<Exam>> examsByScore = CreateExamsByScore();
+
+        public static Dictionary<Student, List<Exam>> CreateExamsByStudent()
+        {
+            Dictionary<Student, List<Exam>> res = new Dictionary<Student, List<Exam>>();
+            foreach (Exam exam in Exams)
+            {
+                if (!res.ContainsKey(exam.Student))
+                {
+                    res[exam.Student] = new List<Exam>();
+                }
+
+                res[exam.Student].Add(exam);
+            }
+            return res;
+        }
+
+        public static Dictionary<Course, List<Exam>> CreateExamsByCourse()
+        {
+            //dit geeft hetzelfde resultaat als de andere twee methoden
+            return Exams.GroupBy(x => x.Course)
+                .ToDictionary(x => x.Key, x => x.ToList());
+        }
+
+        public static Dictionary<decimal, List<Exam>> CreateExamsByScore()
+        {
+            Dictionary<decimal, List<Exam>> res = new Dictionary<decimal, List<Exam>>();
+            foreach (Exam exam in Exams)
+            {
+                if (!res.ContainsKey(exam.Score))
+                {
+                    res[exam.Score] = new List<Exam>();
+                }
+
+                res[exam.Score].Add(exam);
+            }
+            return res;
+        }
+
+        public static decimal GetLowestScoreByStudentName(string naam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<decimal> GetScoreByStudentName(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Proeftentamen/TentamenPrg2Proef20162017.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Proeftentamen
+{
+    public class TentamenPrg2Proef20162017
+    {
+        //Vraag 1 (1.5pt)
+        //Gegeven is een array met getallen (xs), bepaal welk getal het dichtst bij nul ligt.
+        //
+        //Voor het laatste 0.5 punt: een positief getal heeft voorrang op een negatief getal 
+        //(zie het laatste voorbeeld/testcase hieronder). 
+        //Tip: gebruik Math.Abs(..).
+        //Deze methode geeft altijd een positief getal terug.Math.Abs(-3) --> 3, Math.Abs(3) --> 3. 
+        [Test]
+        public void Test_Vraag1()
+        {
+            Assert.AreEqual(0, Vraag1(new int[] { }));
+            Assert.AreEqual(3, Vraag1(new int[] { -5, 4, 3, 8 }));
+            Assert.AreEqual(-3, Vraag1(new int[] { 5, 4, -3, 8 }));
+            Assert.AreEqual(3, Vraag1(new int[] { 5, 3, 4, -3, 8 }));
+        }
+
+        public int Vraag1(int[] xs)
+        {
+            if (xs.Length == 0)
+            {
+                return 0;
+            }
+            int smallest = xs[0];
+            foreach (int i in xs)
+            {
+                if (Math.Abs(i) < Math.Abs(smallest))
+                {
+                    smallest = i;
+                }
+                if (Math.Abs(i) <= Math.Abs(smallest) && i > 0)
+                {
+                    smallest = i;
+                }
+            }
+            return smallest;
+        }
+
+        //Vraag 2 (1.5pt)        
+        //Gegeven is een array met decimalen van een getal (integers). Maak van deze decimale het bijbehorende getal.
+        //Dus b.v. {1, 0, 2, 4} wordt 1024.
+        [Test]
+        public void Test_Vraag2()
+        {
+            Assert.AreEqual(0, Vraag2(new int[] { }));
+            Assert.AreEqual(2, Vraag2(new int[] { 2 }));
+            Assert.AreEqual(25, Vraag2(new int[] { 2, 5 }));
+            Assert.AreEqual(123, Vraag2(new int[] { 1, 2, 3 }));
+            Assert.AreEqual(1024, Vraag2(new int[] { 1, 0, 2, 4 }));
+        }
+
+        public int Vraag2(int[] xs)
+        {
+            int result = 0;
+            for (int i = 0; i < xs.Length; i++)
+            {
+                result *= 10;
+                result += xs[i];
+            }
+            return result;
+        }
+
+        //Vraag 3 (2pt)
+        //Plaats het getal op ide-index in de array vooraan. Je mag geen nieuwe array aanmaken.
+        //i=3  {0, 1, 2, 3, 4, 5} --> {3, 0, 1, 2, 4, 5}
+
+        //In het volgende voorbeeld wordt de waarde 6 die zich op de 2de index in de array 
+        //bevindt vooraan in de array gezet.     
+        //i=2   {0, 5, 6, 7, 8} --> {6, 0, 5, 7, 8}
+
+        //Je mag ervan uit gaan dat de index altijd een geldige waarde bevat.
+
+        [Test]
+        public void Test_Vraag3()
+        {
+            {
+                int[] inputOutput = { 2, 5, 6, 7, 8, -2 };
+                Vraag3(inputOutput, 2);
+                Assert.AreEqual(new int[] { 6, 2, 5, 7, 8, -2 }, inputOutput);
+            }
+            {
+                int[] inputOutput = { 0, 1, 2, 3, 4, 5 };
+                Vraag3(inputOutput, 3);
+                Assert.AreEqual(new int[] { 3, 0, 1, 2, 4, 5 }, inputOutput);
+            }
+
+            {
+                int[] inputOutput = { 0, 5, 6, 7, 8 };
+                Vraag3(inputOutput, 2);
+                Assert.AreEqual(new int[] { 6, 0, 5, 7, 8 }, inputOutput);
+            }
+        }
+
+        public void Vraag3(int[] xs, int index)
+        {
+            int temp = xs[index];
+            for (int i = index; i > 0; i--)
+            {
+                xs[i] = xs[i - 1];
+            }
+            xs[0] = temp;
+        }
+
+        //Vraag 4 (2 pt)
+        //Maak een methode die de volgende reeks berekent: 
+        //2, 5, 26, 577, …
+        //De n-de term kan berekend worden door de vorige term te vermenigvuldigen met zichzelf en er 1 bij te tellen.
+        //De eerste term is 2.
+        //5 = 2*2 + 1
+        //26 = 5*5 + 1
+        //577 = 26*26 + 1
+        //De methode retourneert een lijst van de eerste k termen.
+        //k = 1 --> {2}
+        //k = 2 --> {2, 5}
+        //k = 3 --> {2, 5, 26}
+
+        [Test]
+        public void Test_Vraag4()
+        {
+            Assert.AreEqual(new List<int>() { 2 }, Vraag4(1));
+            Assert.AreEqual(new List<int>() { 2, 5 }, Vraag4(2));
+            Assert.AreEqual(new List<int>() { 2, 5, 26 }, Vraag4(3));
+        }
+
+        public List<int> Vraag4(int k)
+        {
+            int term = 2;
+           List<int> results = new List<int>();
+            for (int i = 0; i < k; i++)
+            {
+                results.Add(term);
+                term = term * term + 1;
+            }
+            return results;
+        }
+
+
+        //Vraag 5 (2pt)
+        //Gegeven is dezelfde studenten database net zoals in het practicum (zie appendix of StudentenDatabase.cs). 
+        //Maak een methode die uitzoekt welke student het meeste examens heeft gedaan. 
+        //Retourneer de naam van deze student.
+        //Opmerking: je mag ervan uitgaan dat er één zo’n student is.  
+
+        //Voor de laatste 0.5 punt doe je dit efficiënt (De StudentNr’s lopen van 1..N, waarbij N het aantal vakken is).  
+        [Test]
+        public void Test_Vraag5()
+        {
+            Assert.AreEqual("Jan", MeesteExamensVoorStudent(StudentDatabase.Students, StudentDatabase.Courses, StudentDatabase.Exams));
+        }
+
+        public string MeesteExamensVoorStudent(List<Student> students,
+                                                List<Course> courses,
+                                                List<Exam> exams)
+        {
+            int maxCount = 0;
+            string nameStudent = "";
+            foreach (Student s in students)
+            {
+                int count = 0;
+                foreach (Exam e in exams)
+                {
+                    if (s == e.Student)
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    nameStudent = s.Name;
+                }
+            }
+            return nameStudent;
+        }
+    }
+}
